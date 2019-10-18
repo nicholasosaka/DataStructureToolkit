cfecbc00f3904f8b1ba8af8d20efe9f28e59c1f4
Nikko Osaka <nikko.osaka99@gmail.com>
2019-10-17 22:18:19 -0400

    scaffolding

--- src/HashTables/Cuckoo.java
+++ src/HashTables/Cuckoo.java
@@ -1,4 +1,25 @@
 package HashTables;
 
-public class Cuckoo {
+import java.util.ArrayList;
+
+/*
+	@author Nikko Osaka
+
+	Implementation of Cuckoo Hashing (Pagh, et al. 2003)
+	Simple dictionary with worst case constant lookup time
+
+ */
+public class Cuckoo<E> {
+
+	//two tables for hashing
+	ArrayList<E> T1 = new ArrayList<E>();
+	ArrayList<E> T2 = new ArrayList<E>();
+
+	//fields
+	private int num = 10;
+	private int size;
+
+	public Cuckoo(){
+
+	}
 }
